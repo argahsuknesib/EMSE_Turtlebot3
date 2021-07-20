036a76a5a7b60ecf22100d6eac6a9ac81e988fe7
Kushagra Singh Bisen <k.bisen@yahoo.com>
2021-07-20 12:14:35 +0200

    added the rosbag to csv script

--- path_planning/emse_eri_robot/scripts/logfile.py
+++ path_planning/emse_eri_robot/scripts/logfile.py
@@ -9,15 +9,12 @@ import csv
 linear_velocity_list = []
 angular_velocity_list = []
 odometry_list = []
-global_costmap_list = []
-goal_list = []
-global_plan_list = []
-clock_list = []
 
 def cmd_vel_callback(msg):
     linear_velocity_x = msg.linear.x
     linear_velocity_y = msg.linear.y
     linear_velocity_z = msg.linear.z
+    rospy.loginfo(linear_velocity_x)
 
     angular_velocity_x = msg.angular.x
     angular_velocity_y = msg.angular.y
@@ -37,6 +34,7 @@ def cmd_vel_callback(msg):
 def odom_callback(msg):
     odom_x = msg.pose.pose.position.x
     odom_y = msg.pose.pose.position.y
+    rospy.loginfo(odom_x,odom_y)
     global odometry_list
     odometry_list.append(odom_x)
     odometry_list.append(odom_y)
@@ -72,20 +70,20 @@ def clock_callback(msg):
 def main():
     rospy.init_node('log_generation_csv', anonymous=True)
     rospy.Subscriber('/odom', Odometry, odom_callback)
-    rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, global_costmap_callback)
-    rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, goal_callback)
-    rospy.Subscriber('/move_base/DWAPlannerROS/global_plan', Path, global_plan_callback)
-    rospy.Subscriber('/clock', Clock, clock_callback)
-    rospy.Subscriber('/cmd_vel', Twist, cmd_vel_callback)
+    # rospy.Subscriber('/move_base/global_costmap/costmap', OccupancyGrid, global_costmap_callback)
+    # rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, goal_callback)
+    # rospy.Subscriber('/move_base/DWAPlannerROS/global_plan', Path, global_plan_callback)
+    # rospy.Subscriber('/clock', Clock, clock_callback)
+    # rospy.Subscriber('/cmd_vel', Twist, cmd_vel_callback)
     with open('/home/whiskygrandee/catkin_ws/src/emse_bot/log/demo-log.csv','w', encoding = 'UTF-8') as f:
         writer = csv.writer(f, delimiter=',', lineterminator= '\n')
         writer.writerow(linear_velocity_list)
         writer.writerow(angular_velocity_list)
         writer.writerow(odometry_list)
-        writer.writerow(global_costmap_list)
-        writer.writerow(goal_list)
-        writer.writerow(global_plan_list)
-        writer.writerow(clock_list)
+        # writer.writerow(global_costmap_list)
+        # writer.writerow(goal_list)
+        # writer.writerow(global_plan_list)
+        # writer.writerow(clock_list)
     rospy.spin()
 
 if __name__ == '__main__':
