90c7a3a85d3876925c352b787f2d3d69c1e44d54
Kushagra Singh Bisen <k.bisen@yahoo.com>
2021-08-03 12:06:58 +0200

    basic working model with accurate manhattan dist

--- path_planning/emse_eri_robot/scripts/manhattan_distance.py
+++ path_planning/emse_eri_robot/scripts/manhattan_distance.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python 
-import rospy 
-from std_msgs import String
-from nav_msgs import Odometry
-from move_base_msgs.msg import MoveBaseGoal
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int32
+from nav_msgs.msg import Odometry
+from move_base_msgs.msg import MoveBaseActionGoal
 
 odom_x = 0
 odom_y = 0
 goal_x = 0
 goal_y = 0
 
-        
+
 def odom_callback(msg):
     global odom_x, odom_y
     odom_x = msg.pose.pose.position.x
@@ -17,8 +17,8 @@ def odom_callback(msg):
 
 def goal_callback(msg):
     global goal_x, goal_y
-    goal_x = msg.target_pose.pose.position.x
-    goal_y = msg.target_pose.pose.position.y
+    goal_x = msg.goal.target_pose.pose.position.x
+    goal_y = msg.goal.target_pose.pose.position.y
 
 def calculate_distance():
     global goal_x, goal_y, odom_x, odom_y
@@ -28,15 +28,15 @@ def calculate_distance():
 def main():
     rospy.init_node('manhattan_distance')
     rospy.Subscriber("/odom", Odometry, odom_callback)
-    rospy.Subscriber("/move_base/goal", MoveBaseGoal, goal_callback)
-    publisher = rospy.Publisher('/manhattan_distance', String, queue_size = 10)
+    rospy.Subscriber("/move_base/goal", MoveBaseActionGoal, goal_callback)
+    publisher = rospy.Publisher('/manhattan_distance', Int32, queue_size = 5)
     distance = calculate_distance()
+    rospy.loginfo(distance)
     publisher.publish(distance)
     rospy.spin()
 
 if __name__ == '__main__':
-    try: 
+    try:
         main()
-    except rospy.ROSInterruptException:
+    except rospy.ROSInterruptException: 
         pass
-    
